0cad7e9f84e87dfa88a365bf6621d41c085041af
fdoalcos <alcosfrancis@gmail.com>
2023-02-06 21:58:42 -0500

    Created a list of objects to be iterated in skillbox component

--- src/components/SkillBox.js
+++ src/components/SkillBox.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 
-let items = ["Python", "JavaScript", "Java", "C", "SQL", "HTML", "CSS", "Django", "Flask", "React"]
 
 function SkillBox({skillImage, skillName}) {
+    const items = ["Python", "JavaScript", "Java", "C", "SQL", "HTML", "CSS", "Django", "Flask", "React"]
+    const item = [{language: "Python", link: "#"}, 
+    {language: "JavaScript", link: "#"}, 
+    {language: "Java", link: "#"}, 
+    {language: "C", link: "#"}, 
+    {language: "SQL", link: "#"}, 
+    {language: "HTML", link: "#"}, 
+    {language: "CSS", link: "#"}, 
+    {language: "Django", link: "#"}, 
+    {language: "Flask", link: "#"},
+    {language: "React", link: "#"} 
+    ]
     return (
         <>
-        {
+        {/* {
             items.map(item => (                
                 <div className='SkillBox'>
                     <div className='skillImage'>
@@ -18,7 +29,20 @@ function SkillBox({skillImage, skillName}) {
                 </div>
             ))
         }
-        
+         */}
+
+        {
+            item.map(item => (                
+                <div className='SkillBox'>
+                    <div className='skillImage'>
+                        <img src={item.link} alt="skillImage"/>
+                    </div>
+                    <div className='skillName'>
+                        <p>{item.language}</p>
+                    </div>
+                </div>
+            ))
+        }
             
             
         </>
