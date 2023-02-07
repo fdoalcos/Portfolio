3387b5544f5d53a23118ad2bd77c014054160192
fdoalcos <alcosfrancis@gmail.com>
2023-02-06 21:16:05 -0500

    some project structure is done

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,31 @@ import React from 'react';
 function Project({date, title, skills, description, link, image}) {
     return (
         <div>
-            
+            <div className='projectContainer'>
+                <div className='projectColumns'>
+                    <div className='projectMain'>
+                        <h3>{date}</h3>
+                        <h1>{title}</h1>
+                        {
+                            skills.map(skill => (
+                                <>
+                                    <div className='skillBox'>
+                                        <h5>{skill}</h5>
+                                    </div>
+                                </>
+                            ))
+                        }
+                        <p>{description}</p>
+                        <div class="projectButton">
+                            <a href={link}>See Project</a>
+                        </div>
+                    </div>
+                    <div className='projectImage'>
+                        <img src={image} alt="projectImage" />
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 }
