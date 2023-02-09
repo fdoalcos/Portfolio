2c62fad36d4ff67b21d7d3b1c0d426b47048db3e
fdoalcos <alcosfrancis@gmail.com>
2023-02-08 21:17:10 -0500

    Adding image to the skill section

--- src/components/SkillBox.js
+++ src/components/SkillBox.js
@@ -3,17 +3,17 @@ import React from 'react';
 
 
 function SkillBox({skillImage, skillName}) {
-    const items = ["Python", "JavaScript", "Java", "C", "SQL", "HTML", "CSS", "Django", "Flask", "React"]
-    const item = [{language: "Python", link: "#"}, 
-    {language: "JavaScript", link: "#"}, 
-    {language: "Java", link: "#"}, 
-    {language: "C", link: "#"}, 
-    {language: "SQL", link: "#"}, 
-    {language: "HTML", link: "#"}, 
-    {language: "CSS", link: "#"}, 
-    {language: "Django", link: "#"}, 
-    {language: "Flask", link: "#"},
-    {language: "React", link: "#"} 
+    const items = ['Python', 'JavaScript', 'Java', 'C', 'SQL', 'HTML', 'CSS', 'Django', 'Flask', 'React']
+    const item = [{language: 'Python', src: '/images/Python.png', className:'imagePython'}, 
+    {language: 'JavaScript', src:'/images/JavaScript.png', className:'imageJavaScript'}, 
+    {language: 'Java', src: '/images/Java.png', className:'imageJava'}, 
+    {language: 'C', src: '/images/C.png', className:'imageC'}, 
+    {language: 'SQL', src: '/images/SQL.png', className:'imageSQL'}, 
+    {language: 'HTML', src: '/images/HTML.png', className:'imageHTML'}, 
+    {language: 'CSS', src: '/images/CSS.png', className:'imageCSS'}, 
+    {language: 'Django', src: '/images/Django.png', className:'imageDjango'}, 
+    {language: 'Flask', src: '/images/Flask.png', className:'imageFlask'},
+    {language: 'React', src: '/images/React.png', className:'imageReact'} 
     ]
     return (
         <>
@@ -35,7 +35,7 @@ function SkillBox({skillImage, skillName}) {
             item.map(item => (                
                 <div className='SkillBox'>
                     <div className='skillImage'>
-                        <img src={item.link} alt="skillImage"/>
+                        <img className={item.className} src={item.src}/>
                     </div>
                     <div className='skillName'>
                         <p>{item.language}</p>
