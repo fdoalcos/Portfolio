76efb98d93d10487eca3e0f808aab1e33a37a32f
fdoalcos <alcosfrancis@gmail.com>
2023-02-26 22:50:14 -0500

    Done with project section

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,16 +9,19 @@ function Project({date, title, skills, description, link, image}) {
                     <div className='projectMain'>
                         <h3 className='projectDate'>{date}</h3>
                         <h1 className='projectTitle'>{title}</h1>
-                        {
-                            skills.map(skill => (
-                                <>
-                                    <div className='skillBox'>
-                                        {/* <h5 className='skillBoxName'>{skill}</h5> */}
-                                        <button className='skillBoxName'>{skill}</button>
-                                    </div>
-                                </>
-                            ))
-                        }
+                        <div className='skillBoxContainer'>
+                            {
+                                skills.map(skill => (
+                                    <>
+                                        <div className='skillsBox'>
+                                            {/* <h5 className='skillBoxName'>{skill}</h5> */}
+                                            <button className='skillBoxName'>{skill}</button>
+                                        </div>
+                                    </>
+                                ))
+                            }
+                        </div>
+                        
                         <p className='projectInfo'>{description}</p>
                         <div class="projectButton">
                             {/* <a href={link}>See Project</a> */}
