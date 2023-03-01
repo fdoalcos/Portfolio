2083410f64573b758054e269b4236d9229e49b72
fdoalcos <alcosfrancis@gmail.com>
2023-03-01 00:15:00 -0500

    done with contact forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from './Button';
 import GradientPurple from './GradientPurple';
+import emailjs from '@emailjs/browser';
+
 
 function Contact(props) {
+    const form = useRef();
     const [email, setEmail] = useState({firstName: '', nameEmail: '', commentInfo: ''})
+    
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_ksj9uhd', 'template_hqvg0me', form.current, 'ShCnZFyJ_5LypBCY2')
+          .then((result) => {
+              console.log(result.text);
+              console.log("message sent");
+          }, (error) => {
+              console.log(error.text);
+              console.log("message error");
+        });
+        
+        document.getElementById("contactForm").reset();
+
+      };
 
     return (
         <div>
@@ -12,20 +31,21 @@ function Contact(props) {
             <GradientPurple />
             
 
-            <div className='contactContainer'>
+            {/* <div className='contactContainer'>
                 <div className='contactColumns'>
                     <div className='contactInput'>
-                        <form method="POST" action="">
+                        <form ref={form} onsubmit={sendEmail}>
                             <div className='contactBorderBottom'>
-                                <input onChange={e => setEmail({...email, firstName: e.target.value})} className='contactButton contactName' type="text" name="Name" placeholder='Your Name' />
+                                <input name="user_name" className='contactButton contactName' type="text" placeholder='Your Name' />
                             </div>
                             <div className='contactBorderBottom'>
-                                <input onChange={e => setEmail({...email, nameEmail: e.target.value})} className='contactButton contactEmail' type="text" name="Email" placeholder='Your Email' />
+                                <input name="user_email"  className='contactButton contactEmail' type="text" placeholder='Your Email' />
                             </div>
                             <div className='contactBorderBottom'>
-                                <input onChange={e => setEmail({...email, commentInfo: e.target.value})} className='contactButton contactThoughts' type="text" name="Thoughts" placeholder='Share Your Thoughts' />
+                                <input name="message" className='contactButton contactThoughts' type="text" placeholder='Share Your Thoughts' />
                             </div>
-                            <Button name={"Submit"} link={"#"} className={"contactSubmit"}/>
+                            <Button name={"Submit"} link={"#"} className={"contactSubmit"}  />
+                            <input type="submit" value="Send" />
                         </form>
                         
                         
@@ -36,7 +56,31 @@ function Contact(props) {
                         <p className='contactInfo'>I am looking for Software Engineer Internships, Web Developer Internships, Software Developer Internships, Full-Stack Developer Internships. I am pleased to show you my website and would love to connect. You can send me a email at alcosfrancis@gmail.com or you can click one of the social media platforms I have and connect!</p>
                     </div>
                 </div>
+            </div> */}
+            <div className='contactContainer'>
+                <div className='contactColumns'>
+                    <div className='contactInput'>
+                        <form id="contactForm" ref={form} onSubmit={sendEmail}>
+                            <div className='contactBorderBottom'>
+                                <input type="text" name="user_name" className='contactButton contactName' placeholder='Your Name' />
+                            </div>
+                            <div className='contactBorderBottom'>
+                                <input type="email" name="user_email"  className='contactButton contactEmail' placeholder='Your Email' />
+                            </div>
+                            <div className='contactBorderBottom'>
+                                <input name="message" className='contactButton contactThoughts' placeholder='Share Your Thoughts' />
+                            </div>
+                            <input type="submit" className='contactSubmit' value="Submit" />
+                        </form>
+                    </div>
+                    <div className='contactHeader'>
+                        <h2 className='contactMainName'>Contacts</h2>
+                        <h1 className='contactSubName'>Want to work together?</h1>
+                        <p className='contactInfo'>I am looking for Software Engineer Internships, Web Developer Internships, Software Developer Internships, Full-Stack Developer Internships. I am pleased to show you my website and would love to connect. You can send me a email at alcosfrancis@gmail.com or you can click one of the social media platforms I have and connect!</p>
+                    </div>
+                </div>
             </div>
+            
         </div>
     );
 }
