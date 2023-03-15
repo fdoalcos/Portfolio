5247ff961681797b0409d2130181a39a04f628e0
fdoalcos <alcosfrancis@gmail.com>
2023-03-14 23:13:50 -0400

    Changing intersection observer

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import GradientBlue from './GradientBlue';
 import GradientPurple from './GradientPurple';
 // import AestheticBrain from './images/AestheticBrain.png'
@@ -9,29 +9,56 @@ import { useInView } from 'react-intersection-observer';
 function AboutMe(props) {
 
 
-    const { ref: aboutRef, inView: isAboutVisible } = useInView();
+    // const { ref: aboutRef, inView: isAboutVisible } = useInView();
 
 
-    console.log("About is", isAboutVisible)
+    // console.log("About is", isAboutVisible)
 
+    const aboutRef = useRef();
+    const [isVisible, setVisible] = useState();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add("showElement")
+                console.log("I am showing element")
+            } else {
+                entry.target.classList.remove("showElement") 
+                console.log("I am not showing element")
+            }
+        })
+    })
+
+    const elements = aboutRef.current.querySelectorAll('.hiddenElement')
+        elements.forEach(element => {
+            observer.observe(element);
+        })
+    }, [])
+
+    
+
+
+    
     return (
         <div>
     
             <GradientPurple />
             <GradientBlue />
             <div>
-                <section ref={aboutRef} className={isAboutVisible ? 'showElement' : 'hideElement' }>
-                    <div className="aboutContainer">
-                        <div className="aboutColumns">
-                            <div className="aboutImages">
-                                <img className="aboutImage" src='/images/AestheticBrain.png'/>
+                <section ref={aboutRef}>
+                    <div className="hiddenElement aboutContainer">
+                        <div className="hiddenElement aboutColumns">
+                            <div className="hiddenElement aboutImages">
+                                <img className="hiddenElement aboutImage" src='/images/AestheticBrain.png'/>
                             </div>
-                            <div className="aboutMainName">
-                                <h1 className="aboutName">About Me</h1>
-                                <h1 className="aboutSubName">I enjoy developing and contributing my skills to the team</h1>
-                                <p className='aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
-                                <p className='aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
-                                <p className='aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
+                            <div className="hiddenElement aboutMainName">
+                                <h1 className="hiddenElement aboutName">About Me</h1>
+                                <h1 className="hiddenElement aboutSubName">I enjoy developing and contributing my skills to the team</h1>
+                                <p className='hiddenElement aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
+                                <p className='hiddenElement aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
+                                <p className='hiddenElement aboutInfo'>I am a Full-Stack Web Developer with passion to grow and contribute my skills to the team. I am currently a first-year student studying Computer Science at University of Massachusetts  Boston and very passionate about development.</p>
                             </div>
                         </div>
                     </div>
