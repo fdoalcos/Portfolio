22e1ecd990ec22b847f731f051e8f1eeff5d7da2
fdoalcos <alcosfrancis@gmail.com>
2023-04-05 23:15:47 -0400

    added resume to link

--- src/components/Home.js
+++ src/components/Home.js
@@ -51,7 +51,7 @@ function Home(props) {
                             <Link to="home" spy={true} smooth={true} offset={-50} duration={500} className='homeLinkHead'>FRANCIS</Link>
                         </a>
                         <div class="hiddenElement flex md:order-2">
-                            <Button name={"Resume"}  link={"resume"}/>
+                            <Button name={"Resume"}  link={"https://docs.google.com/document/d/1VyXNTx9rIzQh-GnGVImYXhGltZN9CA-UA9V2es8P1js/edit?usp=sharing"}/>
                             <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" onClick={toggleClass} aria-expanded="false">
                                 <span class="sr-only">Open main menu</span>
                                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
